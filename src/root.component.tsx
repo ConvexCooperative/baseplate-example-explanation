e8bc845b55f8b7fd9ce785c9bfdac182de654a8d
joeldenning <joeldenning@gmail.com>
2023-12-09 20:53:10 -0700

    Initial commit

---
+++ src/root.component.tsx
@@ -0,0 +1,10 @@
+export default function Root(props) {
+  return (
+    <section style={{maxWidth: "300px", margin: "20px auto"}}>
+      This example Baseplate + single-spa web app was created and deployed in less than 30 minutes.
+      See <a target="_blank" href="https://baseplate.cloud/docs/console/web-apps">Baseplate Documentation</a> for more information. The web app is deployed
+      to multiple Baseplate environments that each have their own domain. The code for this project
+      was generated with <a target="_blank" href="https://single-spa.js.org/docs/create-single-spa">single-spa CLI</a>.
+    </section>
+  );
+}
